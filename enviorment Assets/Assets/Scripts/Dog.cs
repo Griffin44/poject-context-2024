d3b5838b672badb632293a58aed6753eb12d13eb
GodlyHamster <wouter.van.dompselaar@gmail.com>
2024-03-22 11:24:42 +0100

    fixed dog death

--- enviorment Assets/Assets/Scripts/Dog.cs
+++ enviorment Assets/Assets/Scripts/Dog.cs
@@ -5,6 +5,7 @@ public class Dog : MonoBehaviour
     [SerializeField] private Animator dogAnimator;
 
     public void Die() {
+        print("dog should be dead");
         dogAnimator.SetBool("IsDead", true);
     }
 }
--- enviorment Assets/Assets/Scripts/bullet.cs
+++ enviorment Assets/Assets/Scripts/bullet.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    private Vector3 lastPos;
+
+    private void Start()
+    {
+        lastPos = transform.position;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+        print("collided with: " + collision.gameObject.name);
         if (collision.gameObject.TryGetComponent<Health>(out Health objectHealth))
         {
+            print("object has health");
             objectHealth.TakeDamage(1);
         }
         Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        CheckPassthrough();
+    }
+
+    private void CheckPassthrough() { 
+        float distance = (lastPos - transform.position).magnitude;
+        Vector3 direction = (lastPos - transform.position).normalized;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, distance)) {
+            print("raycast hit: " + hit.transform.gameObject.name);
+            if (hit.transform.gameObject.TryGetComponent<Health>(out Health healthObject))
+            {
+                print("object has health");
+                healthObject.TakeDamage(1);
+            }
+            Destroy(gameObject);
+        }
+        lastPos = transform.position;
     }
 }
